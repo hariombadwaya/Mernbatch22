066b493d0078e3b76e21f045d89d9faa00cbe79e
Hariom Badwaya <157871356+hariombadwaya@users.noreply.github.com>
2025-11-28 21:54:34 +0530

    Add files via upload

---
+++ Javascript/Class_7/script.js
@@ -0,0 +1,53 @@
+// WAP to check weather the number is even or not
+
+// let x =11;
+// if(x%2===0){
+//     console.log("The given number is even");
+    
+// }
+// else{
+//     console.log("The given number is odd"); 
+// }
+
+// -------------------------------------------------------------------------
+
+// WAP to print the days 
+ 
+let day = 3
+day= Number(prompt("Enter the number"))
+
+switch(day){
+
+    case 1: 
+    console.log("Monday");
+    break;
+
+    case 2: 
+    console.log("Tuesday");
+    break;
+
+    case 3: 
+    console.log("Wednesday");
+    break;
+
+    case 4: 
+    console.log("Thursday");
+    break;
+
+    case 5: 
+    console.log("Friday");
+    break;
+
+    case 6: 
+    console.log("Saturday");
+    break;
+
+    case 7: 
+    console.log("Sunday");
+    break;
+
+    default:
+    console.log("Invalid day")
+    
+}
+
